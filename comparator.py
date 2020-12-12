aaf956df7ca72a90e99cefdfb1457abdf9b02733
Alex <aliu@tactiohealth.com>
2020-12-12 12:11:25 -0500

    new comparator for 98.261 accuracy

--- comparator.py
+++ comparator.py
@@ -4,12 +4,12 @@ import numpy as np
 #script to determine similarity % between two set of results
 
 #accuracy of champion results to determine if we should submit to kaggle
-champAcc = 96.642
+champAcc = 98.261
 #estimate percentage of champ's wrong predictions that candidate will get right
 upset = 0.5
 
-candidate = pd.read_csv("resultsEx10.csv")
-champ = pd.read_csv("96.csv")
+candidate = pd.read_csv("results400.csv")
+champ = pd.read_csv("98261.csv")
 candidate = np.array(candidate['Label'])
 champ = np.array(champ['Label'])
 
